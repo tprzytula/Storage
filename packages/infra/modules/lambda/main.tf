4797b04eb51808f4576bc9ba3ab4fca0f3ecb9a5
Tomasz Przytula <t.przytula.dev@gmail.com>
2024-02-27 23:02:51 +0000

    Retrieve Lambda sources from S3

--- packages/infra/modules/lambda/main.tf
+++ packages/infra/modules/lambda/main.tf
@@ -8,15 +8,16 @@ resource "aws_iam_role" "lambda_roles" {
 resource "aws_lambda_function" "lambda_functions" {
   for_each = local.lambda_functions
 
-  filename         = format("%s/%s/%s.zip", local.lambda_build_output_directory, each.key, each.key)
-  function_name    = format("%s_%s", each.key, var.random_name)
-  role             = aws_iam_role.lambda_roles[each.key].arn
-  handler          = "index.handler"
-  source_code_hash = filebase64sha256(format("%s/%s/%s.zip", local.lambda_build_output_directory, each.key, each.key))
-  runtime          = "nodejs20.x"
-  memory_size      = "512"
-  publish          = true
-  timeout          = 5
+  function_name     = format("%s_%s", each.key, var.random_name)
+  s3_bucket         = local.s3_bucket_name
+  s3_key            = format("%s/%s.zip", each.key, each.key)
+  s3_object_version = data.aws_s3_bucket_object.lambdas_s3_zips[each.key].version_id
+  role              = aws_iam_role.lambda_roles[each.key].arn
+  handler           = "index.handler"
+  runtime           = "nodejs20.x"
+  memory_size       = "512"
+  publish           = true
+  timeout           = 5
 
   dynamic "environment" {
     for_each = length(keys(each.value.environment_variables)) > 0 ? [each.value.environment_variables] : []
