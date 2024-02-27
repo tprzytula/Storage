f16144d395c04ac8a77d76e22acf1b91c25b028b
Tomasz Przytuła <t.przytula.dev@gmail.com>
2024-02-27 22:13:53 +0000

    Terraform cloud (#6)
    * Publish Lambda dist until better delivery system is implemented
    
    * Change backend to point to terraform cloud
    
    ---------
    
    Co-authored-by: Tomasz Przytula <tprzytula@192.168.1.180>

--- packages/infra/backend.tf
+++ packages/infra/backend.tf
@@ -1,8 +1,9 @@
 terraform {
-  backend "s3" {
-    bucket  = "terraform-state-evvzl7ombf"
-    key     = "state/terraform-aws-household-storage/terraform.tfstate"
-    region  = "eu-west-2"
-    profile = "household-storage"
+  backend "remote" {
+   hostname = "app.terraform.io"
+   organization = "tprzytula"
+   workspaces {
+    name = "Storage"
+   }
   }
 }
